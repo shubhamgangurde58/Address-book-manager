a364cf6cf56231183cb3fb64393d6cf3859b317a
shubhamgangurde58 <shubhamgangurde58@gmail.com>
2024-03-16 19:46:39 +0530

    [ADDED]:shubham Welcome to Address Book manager

---
+++ src/com/shubham/addressbook/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.shubham.addressbook;
+
+public class AddressBookMain {
+
+    public static void main(String[] args){
+        System.out.println("Welcome To Address Book manager");
+    }
+}
